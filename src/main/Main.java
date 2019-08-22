724d7f6daed2eb4fd899207a1243cc6e3e185f26
dell <somiya.parmar_cs18@gla.ac.in>
2019-08-22 15:18:25 +0530

    add output to the screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(system.in);
-        String userName = "";
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine().trim();
+        System.out.println("welcome," + userName + "!");
         scanner.close();
 
 
