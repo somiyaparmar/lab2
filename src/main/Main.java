70fbb455d177c5c9647912155258532d3e885066
dell <somiya.parmar_cs18@gla.ac.in>
2019-08-22 15:33:36 +0530

    final change..

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine().trim();
         System.out.println("welcome," + userName + "!");
-        System.out.println("ho gaya");
+        System.out.println("ho Gaya");
         scanner.close();
 
 
