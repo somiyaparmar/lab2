5661040e8ef2488842c1d248d2bac6bc43069fcb
dell <somiya.parmar_cs18@gla.ac.in>
2019-08-22 15:31:57 +0530

    final change

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 /**
  * @author dell
+ * @version
  */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine().trim();
         System.out.println("welcome," + userName + "!");
+        System.out.println("ho gaya");
         scanner.close();
 
 
