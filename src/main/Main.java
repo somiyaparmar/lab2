add10e4fd470c34203df97d7e6f17e2b95e72d11
dell <somiya.parmar_cs18@gla.ac.in>
2019-08-22 15:13:58 +0530

    object

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author dell
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(system.in);
         String userName = "";
+        scanner.close();
 
 
     }
